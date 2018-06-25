dfccc5f9cc6533a68db485fde2c09b3c4df06659
spacemonkey <nisino@naver.com>
2018-06-25 11:34:16 +0900

    angular2+ mobile web

---
+++ src/app/mail-popup/mail-popup.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
+
+@Component({
+  selector: 'app-mail-popup',
+  templateUrl: './mail-popup.component.html',
+  styleUrls: ['./mail-popup.component.scss']
+})
+export class MailPopupComponent implements OnInit {
+
+  hide:boolean;
+
+  private_class: string = "assets/img/private_class_june.jpg";
+  
+
+  constructor(public _data: DataService) { 
+    if (this._data.centerLocation == 'ss') {
+      this.hide = true;
+    }else if(this._data.centerLocation == 'cd') {
+      this.hide = true;
+    }
+  }
+
+  ngOnInit() {
+  }
+
+  closePopup() {
+    this.hide = true;
+  }
+
+}
